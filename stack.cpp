ddfc8789c960c0989416dbff2e05e1d2f4144721
Ajay Singh <ajaysingh9982374320@gmail.com>
2021-10-09 08:59:30 +0530

    update

---
+++ stack.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int>st;
+    st.push(2);
+    st.push(5);
+    st.push(23);
+    cout<<"The element at the top is: "<<st.top();
+    return 0; 
+}
