7195d03c7fab6ce7d74987e866c20ef187a9185e
Rahul Kumar <rkauraiya1167@gmail.com>
2021-10-09 09:00:16 +0530

    vector code

---
+++ vectors.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+
+    // Vector declaration
+    vector<int>v;
+    
+
+    // Vector push back operations
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+
+
+    for(int i = 0; i < 3; i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+
+    // size function
+    int sz = v.size();
+    cout << v.size() << endl;
+
+    return 0;
+}
